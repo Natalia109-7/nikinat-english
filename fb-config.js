7ab933326c19536a1fa3ad4d1f67b7e14f73839f
Natalia <natalya.tasha2013@gmail.com>
2026-08-15 10:00:15 +0500

    Add Firebase configuration for inforep.ru
    This file contains the Firebase configuration for the site inforep.ru.

---
+++ fb-config.js
@@ -0,0 +1,9 @@
+// Конфиг Firebase сайта inforep.ru. НЕ ИЗМЕНЯТЬ!
+const firebaseConfig = {
+ apiKey: "AIzaSyCXy8bGrYAi9IIp9jhhU1mDpjze1fyE3Zw",
+  authDomain: "nikinat-english.firebaseapp.com",
+  projectId: "nikinat-english",
+  storageBucket: "nikinat-english.firebasestorage.app",
+  messagingSenderId: "820894083136",
+  appId: "1:820894083136:web:24ba3bddbe4f1ba719d7a5"
+};
